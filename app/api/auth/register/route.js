2d1adc7590f6fb0db3c88f6c8eac7d5e735edef5
Stevens Anthony Fabriga <steve-0s@Stevenss-MacBook-Pro.local>
2026-02-27 16:48:15 +0800

    feat(auth): add user signup API and JWT middleware

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,39 @@
+//
+//  route.js
+//  
+//
+//  Created by Stevens Anthony Fabriga on 2/27/26.
+//
+import bcrypt from 'bcrypt'
+import { supabase } from '/Users/steve-0s/Projects/sillagecollective/lib/supabase.js'
+
+export async function POST(req) {
+    try {
+        const { name, username, email, password } = await req.json()
+
+        if (!name || !username || !email || !password) {
+            return Response.json({ error: 'All fields are required' }, { status: 400 })
+        }
+
+        // password hashing
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        // inserting user info to supabase
+        const { error } = await supabaseServer
+            .from('users')
+            .insert({
+                name,
+                username,
+                email,
+                password_hash: hashedPassword
+            })
+
+        if (error) {
+            return Response.json({ error: error.message }, { status: 400 })
+        }
+
+        return Response.json({ message: 'User registered successfully' })
+    } catch (err) {
+        return Response.json({ error: err.message }, { status: 500 })
+    }
+}
---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+import jwt from 'jsonwebtoken'
+
+export function middleware(req) {
+    const token = req.cookies.get('token')?.value
+
+    if (!token) return NextResponse.redirect(new URL('/login', req.url))
+
+    try {
+        jwt.verify(token, process.env.JWT_SECRET)
+        return NextResponse.next()
+    } catch {
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
+}
+
+export const config = {
+    matcher: ['/dashboard/:path*'] // protect /dashboard routes
+}
